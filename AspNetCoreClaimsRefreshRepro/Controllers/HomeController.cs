46956d73543db1529ddf7dc17d70f4fcb841f3cd
Daniel Rusznyak <daniel@rusznyak.com>
2018-06-21 12:06:11 +0100

    Add files for repro

---
+++ AspNetCoreClaimsRefreshRepro/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreClaimsRefreshRepro.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreClaimsRefreshRepro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public HomeController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> AddToRole()
+        {
+            var role = await _roleManager.FindByNameAsync("TestRole");
+            if (role == null)
+            {
+                await _roleManager.CreateAsync(new IdentityRole("TestRole"));
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            await _userManager.AddToRoleAsync(user, "TestRole");
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
